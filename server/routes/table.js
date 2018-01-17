f25f3ce8fe9eb4de60bd57dc55d97d7c889e4986
Brendan Bansavage <brendaniscool@gmail.com>
2018-01-16 18:54:56 -0800

    implement nba box score scraper and send scraped data to client

--- server/routes/table.js
+++ server/routes/table.js
@@ -1,15 +1,47 @@
 var express = require('express');
 var router = express.Router();
+const cheerio = require('cheerio');
+const axios = require ('axios');
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.json([{
-    id: 1,
-    stuff: 'hello'
-  }, {
-    id: 2,
-    stuff: 'hey'
-  }])
+
+  var url = 'http://www.espn.com/nba/matchup?gameId=400975396';
+
+  axios.get(url)
+    .then(res => {
+      let $ = cheerio.load(res.data);
+      var data = '';
+      const cells = $('.mod-data tbody').children('tr').children('td');
+      let obj = cells[4].children[0];
+      let str = obj.data.replace('\n\t\t\t\t\t\t\t\t\t\t', '').replace('\n\t\t\t\t\t\t\t\t\t', '');
+      data += str + ', ';
+      // data += str.split('-')[0] + ', ';
+      // data += str.split('-')[1];
+      console.log(data)
+      return data;
+    })
+    .then(data => {
+      console.log('SUCCESS!');
+      res.json([{
+        id: 1,
+        stuff: data
+      }, {
+        id: 2,
+        stuff: data
+      }])
+    })
+    .catch(err => {
+      console.log('there has been an error: ', err)
+    });
+
+  // res.json([{
+  //   id: 1,
+  //   stuff: 'hello'
+  // }, {
+  //   id: 2,
+  //   stuff: 'hey'
+  // }])
+
 });
 
 module.exports = router;
